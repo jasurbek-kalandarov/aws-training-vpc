9d477246dc2b8989bb3227dfd4642f6c8978a549
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-04-09 16:00:45 +0500

    installed chai

--- tests/vpc.test.js
+++ tests/vpc.test.js
@@ -1,18 +1,23 @@
 import ec2 from "../aws/sdk.js";
-import assert from "assert";
+import { expect } from "chai";
 
 describe('Check stack parameters', ()=> {
   it('should get ec2 metadata',async () => {
     const response = await ec2.describeInstances().promise();
-    const { InstanceType, PrivateIpAddress, VpcId, InstanceId } = response.Reservations[1].Instances[0];
-    const { AvailabilityZone } = response.Reservations[1].Instances[0].Placement;
+    const { 
+      InstanceType, 
+      PrivateIpAddress, 
+      VpcId, 
+      InstanceId,
+      Placement: { AvailabilityZone }
+    } = response.Reservations[1].Instances[0];
     const instanceName = response.Reservations[1].Instances[0].Tags[0].Value;
 
-    assert.equal(instanceName, 'cloudxinfo/PublicInstance/Instance');
-    assert.equal(InstanceId, 'i-0bb1fd9a8786b2fe4');
-    assert.equal(InstanceType, 't2.micro');
-    assert.equal(PrivateIpAddress, '10.0.19.208');    
-    assert.equal(VpcId, 'vpc-0c90de88ba5c55887');
-    assert.equal(AvailabilityZone, 'us-east-1a');
+    expect(instanceName).to.equal('cloudxinfo/PublicInstance/Instance');
+    expect(InstanceId).to.equal('i-0bb1fd9a8786b2fe4');
+    expect(InstanceType).to.equal('t2.micro');
+    expect(PrivateIpAddress).to.equal('10.0.19.208');    
+    expect(VpcId).to.equal('vpc-0c90de88ba5c55887');
+    expect(AvailabilityZone).to.equal('us-east-1a');
   });
 });
