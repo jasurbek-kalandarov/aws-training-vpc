e3935a99ae2002cab739ad7b500412b30ff0d066
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-04-10 13:52:44 +0500

    finished test to download image

---
+++ tests/s3.test.js
@@ -0,0 +1,122 @@
+import { s3} from "../aws/sdk.js";
+import { expect } from "chai";
+import FormData from "form-data";
+import fs from 'fs';
+import httpRequest from "../utils/httpRequester.js";
+import  getListOfBucketsContainingName from "../utils/get-bucket-list.js"
+import { readdir } from 'fs/promises';
+
+describe('Check S3 app metadata', () => {
+  it('should get a list of S3 buckets', async () => {
+    const data = await s3.listBuckets().promise();
+    const hasCloudxBuckets = data.Buckets.some(bucket => bucket.Name.includes('cloudx'));
+    expect(hasCloudxBuckets).to.equal(true);
+  });
+
+  it('should get tags for buckets', async () => {
+    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
+    
+    for (const bucket of cloudxBuckets) {
+      const resp = await s3.getBucketTagging({ Bucket: bucket.Name }).promise();
+      const [ cloudxTag ] = resp.TagSet.filter(tagName => tagName.Key === 'cloudx');
+
+      expect(cloudxTag.Key).to.equal('cloudx');
+      expect(cloudxTag.Value).to.equal('qa');
+    }
+  });
+
+  it('should get bucket Encryption', async () => {
+    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
+
+    for (const bucket of cloudxBuckets) {
+      const resp = await s3.getBucketEncryption({ Bucket: bucket.Name }).promise();
+      const { BucketKeyEnabled } = resp.ServerSideEncryptionConfiguration.Rules[0];
+
+      expect(BucketKeyEnabled).to.be.false;
+      expect(BucketKeyEnabled).to.be.false;
+    }
+  });
+
+  it('should get bucket versioning', async () => {
+    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
+
+    for (const bucket of cloudxBuckets) {
+      const resp = await s3.getBucketVersioning({ Bucket: bucket.Name }).promise();
+      expect(resp.Status).to.be.undefined;
+    }
+  });
+
+  it('should get bucket public access', async () => {
+    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
+
+    for (const bucket of cloudxBuckets) {
+      const resp = await s3.getBucketAcl({ Bucket: bucket.Name }).promise();
+
+      expect(resp.Grants[0].Grantee.DisplayName).to.equal('jkalandarov');
+      expect(resp.Grants[0].Permission).to.equal('FULL_CONTROL');
+    }
+  });
+});
+
+describe('Checkt S3 app functionality', () => {
+  it('should upload an image to bucket', async () => {
+    const data = new FormData();
+    data.append('upfile', fs.createReadStream('./screenshots/test report.jpg'));
+
+    const config = {
+      url: 'http://52.90.88.242/api/image',
+      method: 'post',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data
+    }
+
+    const resp = await httpRequest(config);
+    expect(resp.status).to.equal(204);
+  });
+
+  it('should get all images', async () => {
+    const config = {
+      url: 'http://52.90.88.242/api/image',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const resp = await httpRequest(config);
+
+    const expectedKeys = ['id', 'last_modified', 'object_key', 'object_size', 'object_type'];
+
+    expect(resp.status).to.equal(200);
+    expect(resp.data.length).to.greaterThan(0);
+    expect(resp.data[0]).to.have.all.keys(expectedKeys);
+  });
+
+  it.only('should download an image by id', async () => {
+    const config = {
+      url: 'http://52.90.88.242/api/image/file/1',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const resp = await httpRequest(config);
+
+    //Check response status and data
+    expect(resp.status).to.equal(200);
+    expect(resp.data).to.be.true;
+
+    const downloadedFile = fs.createWriteStream('./downloads/downdload.jpg');
+    downloadedFile.write(resp.data);
+
+    let listOfFiles = await readdir('./downloads/');
+    //Check file is successfully downloaded to the folder
+    listOfFiles.forEach(fileName => {
+      expect(fileName).to.match(/\w+\.jpg/);
+    })
+  });
+  
+});
--- tests/vpc.test.js
+++ tests/vpc.test.js
@@ -1,11 +1,8 @@
-import { ec2, s3} from "../aws/sdk.js";
+import { ec2 } from "../aws/sdk.js";
 import { expect } from "chai";
-import FormData from "form-data";
-import fs from 'fs';
-import httpRequest from "../utils/httpRequester.js";
-import  getListOfBucketsContainingName from "../utils/get-bucket-list.js";
+;
 
-describe('Check stack parameters', ()=> {
+describe.skip('Check stack parameters', ()=> {
   it('should get ec2 metadata',async () => {
     const response = await ec2.describeInstances().promise();
     const { 
@@ -25,95 +22,3 @@ describe('Check stack parameters', ()=> {
     expect(AvailabilityZone).to.equal('us-east-1a');
   });
 });
-
-describe('Check S3 app metadata', () => {
-  it('should get a list of S3 buckets', async () => {
-    const data = await s3.listBuckets().promise();
-    const hasCloudxBuckets = data.Buckets.some(bucket => bucket.Name.includes('cloudx'));
-    expect(hasCloudxBuckets).to.equal(true);
-  });
-
-  it('should get tags for buckets', async () => {
-    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
-    
-    for (const bucket of cloudxBuckets) {
-      const resp = await s3.getBucketTagging({ Bucket: bucket.Name }).promise();
-      const [ cloudxTag ] = resp.TagSet.filter(tagName => tagName.Key === 'cloudx');
-
-      expect(cloudxTag.Key).to.equal('cloudx');
-      expect(cloudxTag.Value).to.equal('qa');
-    }
-  });
-
-  it('should get bucket Encryption', async () => {
-    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
-
-    for (const bucket of cloudxBuckets) {
-      const resp = await s3.getBucketEncryption({ Bucket: bucket.Name }).promise();
-      const { BucketKeyEnabled } = resp.ServerSideEncryptionConfiguration.Rules[0];
-
-      expect(BucketKeyEnabled).to.be.false;
-      expect(BucketKeyEnabled).to.be.false;
-    }
-  });
-
-  it('should get bucket versioning', async () => {
-    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
-
-    for (const bucket of cloudxBuckets) {
-      const resp = await s3.getBucketVersioning({ Bucket: bucket.Name }).promise();
-      expect(resp.Status).to.be.undefined;
-    }
-  });
-
-  it('should get bucket public access', async () => {
-    const cloudxBuckets = await getListOfBucketsContainingName('cloudx');
-
-    for (const bucket of cloudxBuckets) {
-      const resp = await s3.getBucketAcl({ Bucket: bucket.Name }).promise();
-
-      expect(resp.Grants[0].Grantee.DisplayName).to.equal('jkalandarov');
-      expect(resp.Grants[0].Permission).to.equal('FULL_CONTROL');
-    }
-  });
-});
-
-describe.only('Checkt S3 app functionality', () => {
-  it('should upload an image to bucket', async () => {
-    const data = new FormData();
-    data.append('upfile', fs.createReadStream('./screenshots/test report.jpg'));
-
-    const config = {
-      url: 'http://52.90.88.242/api/image',
-      method: 'post',
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      data
-    }
-
-    const resp = await httpRequest(config);
-    expect(resp.status).to.equal(204);
-  });
-
-  it.only('should get all images', async () => {
-    const config = {
-      url: 'http://52.90.88.242/api/image',
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-
-    const resp = await httpRequest(config);
-
-    expect(resp.status).to.equal(200);
-    expect(resp.data.length).to.greaterThan(0);
-    expect(resp.data[0]).to.have.all.keys(['id', 'last_modified', 'object_key', 'object_size', 'object_type']);
-  });
-  
-});
-
-
-// const response = await httpRequest(config);
-// console.log('****', data);
