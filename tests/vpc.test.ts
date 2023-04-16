6c340f191115ff612a5cb6c4faedc8de348f9327
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-04-16 14:33:19 +0500

    finished VPC tests

--- tests/vpc.test.ts
+++ tests/vpc.test.ts
@@ -1,24 +1,68 @@
+import { EC2 } from "aws-sdk";
 import { ec2 } from "../aws/sdk";
 import { expect } from "chai";
-;
+import { CloudxInfo } from "../utils/interfaces";
+import { readJsonData } from "../utils/readData";
 
-describe.skip('Check stack parameters', ()=> {
-  it('should get ec2 metadata',async () => {
-    const response = await ec2.describeInstances().promise();
-    const { 
-      InstanceType, 
-      PrivateIpAddress, 
-      VpcId, 
-      InstanceId,
-      // Placement: { AvailabilityZone }
-    } = response!.Reservations![1].Instances![0];
-    const instanceName = response!.Reservations![1].Instances![0].Tags![0].Value;
+let cloudxinfo: CloudxInfo;
 
-    expect(instanceName).to.equal('cloudxinfo/PublicInstance/Instance');
-    expect(InstanceId).to.equal('i-0a1ea4ac7bcdbd937');
-    expect(InstanceType).to.equal('t2.micro');
-    expect(PrivateIpAddress).to.equal('10.0.4.130');    
-    expect(VpcId).to.equal('vpc-08b83990e0464a2ac');
-    // expect(AvailabilityZone).to.equal('us-east-1a');
+describe.only('VPC', ()=> {
+  let VpcId: string | undefined;
+  let CidrBlock: string | undefined;
+  let State: string | undefined;
+  let Tags: EC2.TagList | undefined;
+
+  describe('First VPC', () => {
+    before(async () => {
+      const { Vpcs } = await ec2.describeVpcs().promise();
+      ({ VpcId, CidrBlock, State, Tags} = Vpcs![0]);
+  
+      expect(Vpcs?.length).to.equal(2);
+  
+      cloudxinfo = await readJsonData("cloudxinfo");
+    });
+  
+    it('should have VpcId', () => {
+      expect(cloudxinfo.vpcs![0].vpcId).to.equal(VpcId);
+    });
+  
+    it('should have vpc cidr block', () => {
+      expect(cloudxinfo.vpcs![0].cidrBlock).to.equal(CidrBlock);
+    });
+  
+    it('vpc state should be available', () => {
+      expect(cloudxinfo.vpcs![0].state).to.equal(State);
+    });
+  
+    it('should have tags', () => {
+      expect(cloudxinfo.vpcs![0].tags).to.deep.equal(Tags);
+    });
+  });
+
+  describe('Second VPC', () => {
+    before(async () => {
+      const { Vpcs } = await ec2.describeVpcs().promise();
+      ({ VpcId, CidrBlock, State, Tags} = Vpcs![1]);
+  
+      expect(Vpcs?.length).to.equal(2);
+  
+      cloudxinfo = await readJsonData("cloudxinfo");
+    });
+  
+    it('should have VpcId', () => {
+      expect(cloudxinfo.vpcs![1].vpcId).to.equal(VpcId);
+    });
+  
+    it('should have vpc cidr block', () => {
+      expect(cloudxinfo.vpcs![1].cidrBlock).to.equal(CidrBlock);
+    });
+  
+    it('vpc state should be available', () => {
+      expect(cloudxinfo.vpcs![1].state).to.equal(State);
+    });
+  
+    it('should have tags', () => {
+      expect(cloudxinfo.vpcs![1].tags).to.deep.equal(Tags);
+    });
   });
 });
