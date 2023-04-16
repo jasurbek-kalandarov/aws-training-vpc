3d97c3659f64cb40e4e4feb375fdf8fa8c115950
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-04-16 14:46:10 +0500

    created rds helper

--- aws/sdk.ts
+++ aws/sdk.ts
@@ -13,5 +13,6 @@ const params = { region: 'us-east-1', apiVersion: '2016-11-15' }
 
 const ec2 = new AWS.EC2(params);
 const s3 = new AWS.S3(params);
+const rds = new AWS.RDS(params);
 
-export { ec2, s3 };
+export { ec2, s3, rds };
