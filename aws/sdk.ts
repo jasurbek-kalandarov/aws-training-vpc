f4602b344b50e635ea61a02bc00a387208091aeb
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-04-16 20:20:16 +0500

    created rdsQuery

--- aws/sdk.ts
+++ aws/sdk.ts
@@ -14,5 +14,6 @@ const params = { region: 'us-east-1', apiVersion: '2016-11-15' }
 const ec2 = new AWS.EC2(params);
 const s3 = new AWS.S3(params);
 const rds = new AWS.RDS(params);
+const rdsQuery = new AWS.RDSDataService({ region: 'us-east-1' });
 
-export { ec2, s3, rds };
+export { ec2, s3, rds, rdsQuery };
