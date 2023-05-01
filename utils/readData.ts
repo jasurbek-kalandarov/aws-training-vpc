89a29d83ef2b9fbbf5253c314c923c484f50ad31
Jasurbek Kalandarov <jasurbek_kalandarov@epam.com>
2023-05-01 23:00:43 +0500

    updated readJsonData

--- utils/readData.ts
+++ utils/readData.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 
-export async function readJsonData(fileName: 'cloudxinfo' | 'cloudximage') {
+export async function readJsonData(fileName: 'cloudxinfo' | 'cloudximage' | 'cloudxserverless') {
   const data = await fs.readFile(`./data/${fileName}Data.json`, { encoding: 'utf-8' });
   return JSON.parse(data);
 };
